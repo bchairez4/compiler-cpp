0d438b84b2adc02f98380b67ef6db7db53c62334
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-10-19 20:57:26 -0700

    added private data and class constructors

--- LexicalAnalyzer.h
+++ LexicalAnalyzer.h
@@ -1,11 +1,25 @@
 #ifndef LEXICALANALYZER_H
 #define LEXICALANALYZER_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+
 class LexicalAnalyzer {
     private:
-        //
+        char alphaCharacters_[27] = {'_', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        char digits_[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        char separators_[8] = {',', ';', '{', '}', '(', ')', '[', ']'};
+        std::string keywords_[13] = {"bool", "char", "do", "double", "else", "false", "for", "float", "int", "if", "return", "true", "while"};
+        std::string operators_[7] = {"=", "==", "!=", "<", ">", "<=", ">="};
     public:
-        //
+        LexicalAnalyzer() {}
+
+        LexicalAnalyzer(const LexicalAnalyzer& other) {} 
+
+        ~LexicalAnalyzer() {}
+
+        LexicalAnalyzer& operator=(const LexicalAnalyzer other) {}
 };
 
 #endif
