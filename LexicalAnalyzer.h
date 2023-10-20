c6d402dc86fb8caa844667688edbd6135e4eba55
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-10-19 20:59:00 -0700

    updated parameter for assignment operator overloading

--- LexicalAnalyzer.h
+++ LexicalAnalyzer.h
@@ -19,7 +19,7 @@ class LexicalAnalyzer {
 
         ~LexicalAnalyzer() {}
 
-        LexicalAnalyzer& operator=(const LexicalAnalyzer other) {}
+        LexicalAnalyzer& operator=(const LexicalAnalyzer& other) {}
 };
 
 #endif
