380650981d9f82e946ac94407d35ef3c9225f046
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-10-20 20:00:15 -0700

    implemented MeaningfulUnit.h

--- MeaningfulUnit.h
+++ MeaningfulUnit.h
@@ -1,12 +1,43 @@
 #ifndef MEANINGFULUNIT_h
 #define MEANINGFULUNIT_H
 
+#include <string>
+
 class MeaningfulUnit {
     private:
-        //
+        std::string token_;
+        std::string lexeme_;
     public:
-        //
+        MeaningfulUnit() : token_("unknown"), lexeme_("unknown") {}
 
+        MeaningfulUnit(const std::string& token, const std::string& lexeme) : token_(token), lexeme_(lexeme) {}
+
+        MeaningfulUnit(const MeaningfulUnit& other) : token_(other.token_), lexeme_(other.lexeme_) {}
+
+        ~MeaningfulUnit() {}
+
+        MeaningfulUnit& operator=(const MeaningfulUnit& other) {
+            token_ = other.token_;
+            lexeme_ = other.lexeme_;
+
+            return *this;
+        }
+
+        std::string getToken() const {
+            return token_;
+        }
+
+        std::string getLexeme() const {
+            return lexeme_;
+        }
+
+        void setToken(const std::string& token) {
+            token_ = token;
+        }
+
+        void setLexeme(const std::string& lexeme) {
+            lexeme_ = lexeme;
+        } 
 };
 
 #endif
