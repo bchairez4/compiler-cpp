707ba075d0bbfda3fd703abde4b7ecdaad79540c
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-10-25 18:31:10 -0700

    Create SyntacticalAnalyzer.h

---
+++ SyntacticalAnalyzer.h
@@ -0,0 +1,19 @@
+#ifndef SYNTACTICALANALYZER_H
+#define SYNTACTICALANALYZER_H
+
+class SyntacticalAnalyzer {
+    private:
+        //
+    public:
+        SyntacticalAnalyzer() {}
+
+        SyntacticalAnalyzer(const SyntacticalAnalyzer& other) {}
+
+        ~SyntacticalAnalyzer() {}
+
+        SyntacticalAnalyzer& operator=(const SyntacticalAnalyzer& other) {
+            return *this;
+        }
+};
+
+#endif
