7d4de893f9df49c287464f470beb21f69e62c36a
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-10-27 21:04:40 -0700

    Create SemanticAnalyzer.h

---
+++ SemanticAnalyzer.h
@@ -0,0 +1,19 @@
+#ifndef SEMANTICANALYZER_H
+#define SEMANTICANALYZER_H
+
+class SemanticAnalyzer {
+    private:
+        //
+    public:
+        SemanticAnalyzer() {}
+
+        SemanticAnalyzer(const SemanticAnalyzer& other) {}
+
+        ~SemanticAnalyzer() {}
+
+        SemanticAnalyzer& operator=(const SemanticAnalyzer& other) {
+            return *this;
+        }
+};
+
+#endif
