352d796c157ecd79611b6f39a93d0a1c4ae3c661
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-10-27 21:03:12 -0700

    update class information

---
+++ SyntaxAnalyzer.h
@@ -0,0 +1,19 @@
+#ifndef SYNTAXANALYZER_H
+#define SYNTAXANALYZER_H
+
+class SyntaxAnalyzer {
+    private:
+        //
+    public:
+        SyntaxAnalyzer() {}
+
+        SyntaxAnalyzer(const SyntaxAnalyzer& other) {}
+
+        ~SyntaxAnalyzer() {}
+
+        SyntaxAnalyzer& operator=(const SyntaxAnalyzer& other) {
+            return *this;
+        }
+};
+
+#endif
