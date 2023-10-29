19dc71e2e2878dbf8738112776bf501fd0f52603
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-10-28 20:11:21 -0700

    Create IntermediateCodeGenerator.h

---
+++ IntermediateCodeGenerator.h
@@ -0,0 +1,17 @@
+#ifndef INTERMEDIATECODEGENERATOR_H
+#define INTERMEDIATECODEGENERATOR_H
+
+class IntermediateCodeGenerator {
+    private:
+        //
+    public:
+        IntermediateCodeGenerator() {}
+
+        IntermediateCodeGenerator(const IntermediateCodeGenerator& other) {}
+
+        ~IntermediateCodeGenerator() {}
+
+        IntermediateCodeGenerator& operator=(const IntermediateCodeGenerator& other) {}
+};
+
+#endif
