b2b52986baf76416e27759ff287d28ba56098815
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-10-28 20:12:53 -0700

    included helper files

--- Compiler.h
+++ Compiler.h
@@ -1,6 +1,11 @@
 // C++ compiler 
 // Created by Brian Chairez
 
+#include "LexicalAnalyzer.h"
+#include "SyntaxAnalyzer.h"
+#include "SemanticAnalyzer.h"
+#include "IntermediateCodeGenerator.h"
+
 class Compiler {
     private:
         //
